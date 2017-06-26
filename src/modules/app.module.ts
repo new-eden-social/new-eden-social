17c9c53b2f8d115783f21c51b7c6dd94acaca19f
Tine Jozelj <mentos1386@outlook.com>
2017-06-26 18:53:49 +0200

    clean old code

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
 
-import { BoardsModule } from './boards/boards.module';
-import { ThreadsModule } from './threads/threads.module';
-
 @Module({
   modules: [
-    BoardsModule,
-    ThreadsModule,
   ],
 })
 export class ApplicationModule {
