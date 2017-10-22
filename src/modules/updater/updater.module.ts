0e8c0265d7b5bf53c11bdc9a0b7eb14191d5a292
Tine Jozelj <tine.jozelj@tjo.space>
2017-10-22 18:04:25 +0200

    implement corporation updating in updater

--- src/modules/updater/updater.module.ts
+++ src/modules/updater/updater.module.ts
@@ -3,14 +3,18 @@ import { UpdaterService } from './updater.service';
 import { CharacterModule } from '../character/character.module';
 import { DatabaseModule } from '../database/database.module';
 import { characterProviders } from '../character/character.providers';
+import { CorporationModule } from '../corporation/corporation.module';
+import { corporationProviders } from '../corporation/corporation.providers';
 
 @Module({
   modules: [
     CharacterModule,
+    CorporationModule,
     DatabaseModule,
   ],
   components: [
     ...characterProviders,
+    ...corporationProviders,
     UpdaterService,
   ],
 })
--- src/modules/updater/updater.service.ts
+++ src/modules/updater/updater.service.ts
@@ -4,17 +4,22 @@ import { Character } from '../character/character.entity';
 import { CHARACTER_REPOSITORY_TOKEN } from '../character/character.constants';
 import { Repository } from 'typeorm';
 import Log from '../../utils/Log';
+import { CorporationService } from '../corporation/corporation.service';
+import { CORPORATION_REPOSITORY_TOKEN } from '../corporation/corporation.constants';
+import { Corporation } from '../corporation/corporation.entity';
 
 @Component()
 export class UpdaterService {
 
   private readonly LOOP_INTERVAL = 60000; // 1min timeout
-  private readonly UPDATE_INTERVAL = '1 day';
+  private readonly UPDATE_INTERVAL = '1 hour';
   private readonly UPDATE_LIMIT = 100;
 
   constructor(
     @Inject(CHARACTER_REPOSITORY_TOKEN) private characterRepository: Repository<Character>,
+    @Inject(CORPORATION_REPOSITORY_TOKEN) private corporationRepository: Repository<Corporation>,
     private characterService: CharactersService,
+    private corporationService: CorporationService,
   ) {
     this.loop();
     setInterval(this.loop.bind(this), this.LOOP_INTERVAL);
@@ -24,9 +29,12 @@ export class UpdaterService {
    * Main Loop
    */
   private loop(): void {
-    this.updateCharacters()
+    Promise.all([
+      this.updateCharacters(),
+      this.updateCorporations(),
+    ])
     .then(() => {
-      Log.debug('Update loop done');
+      Log.info('Update loop done');
     });
   }
 
@@ -49,6 +57,28 @@ export class UpdaterService {
     idsStream.on('data', ({ id }) => {
       this.characterService.get(id)
       .then(character => this.characterService.update(character));
+    });
+  }
+
+  /**
+   * Update Corporations
+   * @return {Promise<void>}
+   */
+  private async updateCorporations(): Promise<void> {
+    const idsStream = await this.corporationRepository
+    .createQueryBuilder('corporation')
+    .select('id')
+    .where(`"updatedAt" < (NOW() - interval '${this.UPDATE_INTERVAL}')`)
+    .limit(this.UPDATE_LIMIT)
+    .stream();
+
+    idsStream.on('error', (err) => {
+      throw err;
+    });
+
+    idsStream.on('data', ({ id }) => {
+      this.corporationService.get(id)
+      .then(corporation => this.corporationService.update(corporation));
     });
   }
 
