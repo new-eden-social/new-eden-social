071daa98164ed0faf62e5510fa5ad0b2c46ab55d
Tine Jozelj <tine.jozelj@tjo.space>
2017-10-29 16:47:10 +0100

    add modules to app.module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -5,13 +5,17 @@ import { AuthenticationModule } from './authentication/authentication.module';
 import { PostModule } from './post/post.module';
 import { MiddlewaresConsumer } from '@nestjs/common/interfaces/middlewares';
 import { RequestContextMiddleware } from './requestContext/requestContext.middleware';
+import { AllianceModule } from './alliance/alliance.module';
+import { CorporationModule } from './corporation/corporation.module';
 
 @Module({
   modules: [
     SearchModule,
+    AllianceModule,
     CharacterModule,
-    AuthenticationModule,
+    CorporationModule,
     PostModule,
+    AuthenticationModule,
   ],
 })
 export class ApplicationModule {
