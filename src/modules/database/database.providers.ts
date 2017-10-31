824402dd1b81e0f1e6b00539eaae3fdaefaa81fb
Tine Jozelj <tine.jozelj@tjo.space>
2017-10-31 23:01:40 +0100

    add redis_url to database cache

--- src/modules/database/database.providers.ts
+++ src/modules/database/database.providers.ts
@@ -21,6 +21,7 @@ export const databaseProviders = [
       cache: {
         type: 'redis',
         options: {
+          url: process.env.REDIS_URL,
           host: process.env.REDIS_HOST,
           port: process.env.REDIS_PORT,
         },
