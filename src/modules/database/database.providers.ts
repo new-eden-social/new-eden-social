9dbc0e1b1baaeec84f3974ef0238a75df0bf9144
Tine Jozelj <tine.jozelj@tjo.space>
2017-11-01 09:19:22 +0100

    fix db connection

--- src/modules/database/database.providers.ts
+++ src/modules/database/database.providers.ts
@@ -6,10 +6,30 @@ export const databaseProviders = [
   {
     provide: DB_CONNECTION_TOKEN,
     useFactory: async () => {
-      console.log('setting database', process.env.REDIS_URL);
-      const dbCon = await createConnection({
+      // Connect with URL
+      if (process.env.DB_URL)
+        return await createConnection({
+          type: 'postgres',
+          url: process.env.DB_URL,
+          logging: <LoggerOptions>process.env.DB_LOG,
+          entities: [
+            __dirname + '/../**/*.entity.ts',
+          ],
+          synchronize: process.env.DB_SYNC === 'true',
+          cache: {
+            type: 'redis',
+            options: {
+              url: process.env.REDIS_URL,
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            },
+            alwaysEnabled: process.env.DB_ALWAYS_CACHE === 'true',
+          },
+        });
+
+      // Connect with host/port/username/password...
+      return await createConnection({
         type: 'postgres',
-        url: process.env.DB_URL,
         host: process.env.DB_HOST,
         port: parseInt(process.env.DB_PORT, 10),
         username: process.env.DB_USERNAME,
@@ -20,18 +40,16 @@ export const databaseProviders = [
           __dirname + '/../**/*.entity.ts',
         ],
         synchronize: process.env.DB_SYNC === 'true',
-        /*      cache: {
-                type: 'redis',
-                options: {
-                  url: process.env.REDIS_URL,
-                  host: process.env.REDIS_HOST,
-                  port: process.env.REDIS_PORT,
-                },
-                alwaysEnabled: process.env.DB_ALWAYS_CACHE === 'true',
-              },*/
+        cache: {
+          type: 'redis',
+          options: {
+            url: process.env.REDIS_URL,
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT,
+          },
+          alwaysEnabled: process.env.DB_ALWAYS_CACHE === 'true',
+        },
       });
-      console.log('db con set up');
-      return dbCon;
     },
   },
 ];
